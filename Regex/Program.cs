7f058fb53d170e60cc8e1de6788e83ac19e00561
gitakshay1 <akshaypatil309p8@gmail.com>
2023-01-24 16:44:15 +0530

    [Akshay] Added program for matching last name

--- Regex/Program.cs
+++ Regex/Program.cs
@@ -12,6 +12,9 @@
                 case 1:
                     regex1.FirstName();
                     break;
+                case 2:
+                    regex1.LastName();
+                    break;
                 default:
                     Console.WriteLine("Enter Valid choise");
                     break;
--- Regex/Regex1.cs
+++ Regex/Regex1.cs
@@ -13,19 +13,38 @@ namespace Regex_Programs
         public void FirstName()
         {  
             Console.WriteLine("Enter Firts Name");
-            var data = Console.ReadLine();
+            string data = Console.ReadLine();
             string pattern = "^[A-Z]{1}[a-z]{2,}";
             Regex regex = new Regex(pattern);
             if (regex.IsMatch(data))
             {
                 Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine(data + " is a Valid Name");
+                Console.WriteLine(data + " is a Valid First Name");
                 Console.ResetColor();
             }
             else
             {
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine(data + " is a Invalid Name");
+                Console.WriteLine(data + " is a Invalid First Name");
+                Console.ResetColor();
+            }
+        }
+        public void LastName()
+        {
+            Console.WriteLine("Enter first name");
+            string data = Console.ReadLine();
+            string pattern = "^[A-Z]{1}[a-z]{2,}";
+            Regex regex = new Regex(pattern);
+            if (regex.IsMatch(data))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(data + " is a Valid Last Name");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(data + " is a Invalid Last Name");
                 Console.ResetColor();
             }
         }
