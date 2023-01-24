09b887c41b2ccfd7158f6cbfc09b8b34826573ae
gitakshay1 <akshaypatil309p8@gmail.com>
2023-01-24 16:35:44 +0530

    [Akshay] Added program for matching first name

---
+++ Regex/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Regex_Programs
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the choise\n1 First Name");
+            int choise = Convert.ToInt32(Console.ReadLine());
+            Regex1 regex1=new Regex1();
+            switch(choise)
+            {
+                case 1:
+                    regex1.FirstName();
+                    break;
+                default:
+                    Console.WriteLine("Enter Valid choise");
+                    break;
+
+            }
+            
+        }
+    }
+}
